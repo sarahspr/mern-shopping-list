c34fc175d830d35eea95465f85de17fcc22520db
Sarah Spriggs <sarahspriggs@Sarahs-MacBook-Pro.local>
2021-05-07 16:44:19 -0400

    Edited server.js to prepare for Heroku deployment.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const path = require('path');
 const items = require('./routes/api/items');
 
 const app = express();
@@ -20,6 +20,16 @@ mongoose
 
 //Use Routes
 app.use('/api/items', items);
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Setup port for server to run on
 const port = process.env.PORT || 5000;
